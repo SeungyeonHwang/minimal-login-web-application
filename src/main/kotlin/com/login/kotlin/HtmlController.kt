2910fe6f9630b904cca39cdf234dc955aa19397f
HwangSeungyeon <syhwang.web@gmail.com>
2021-02-08 22:07:48 +0900

    #root page setting #mustache templete setting

--- src/main/kotlin/com/login/kotlin/HtmlController.kt
+++ src/main/kotlin/com/login/kotlin/HtmlController.kt
@@ -1,5 +1,15 @@
 package com.login.kotlin
 
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.RequestMapping
+
+@Controller
 class HtmlController {
 
+    //root page
+    @RequestMapping("/")
+    fun index(model: Model): String {
+            return "index"
+    }
 }
