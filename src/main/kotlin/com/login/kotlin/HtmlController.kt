8aa8e85925454793212c27e8917498f8319a3b38
HwangSeungyeon <syhwang.web@gmail.com>
2021-02-10 23:13:13 +0900

    #Sign Page #Login Page #Mustacheを利用して重複削除 #URLMapping

--- src/main/kotlin/com/login/kotlin/HtmlController.kt
+++ src/main/kotlin/com/login/kotlin/HtmlController.kt
@@ -2,14 +2,32 @@ package com.login.kotlin
 
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
+import kotlin.math.sign
 
 @Controller
 class HtmlController {
 
     //root page
-    @RequestMapping("/")
+    @GetMapping("/")
     fun index(model: Model): String {
-            return "index"
+        model.addAttribute("title", "Home")
+        return "index"
+    }
+
+    @GetMapping("/{formType}")
+    fun htmlForm(model: Model, @PathVariable formType: String): String {
+        var response: String = ""
+
+        if (formType.equals("sign")) {
+            response = "sign"
+        } else if (formType.equals("login")) {
+            response = "login"
+        }
+        model.addAttribute("title", response)
+
+        return response
     }
 }
